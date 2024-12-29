ccbd1bda7c9f3dc727b202519b5a859298838d42
Tim0nka <timonvaismann@tutamail.com>
2024-12-30 01:12:09 +0300

    Add files via upload

--- notation.py
+++ notation.py
@@ -1,15 +1,12 @@
+nums = {10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'}
+
 def notation(N, T):
     if T > 16:
         return 'Основание системы счисления должно быть не больше 16-ти'
 
-    number = ''
-    while N > 0:
-        N, r = divmod(N, T)
-        if r > 9:
-            r = chr(ord('A') + r - 10)
-        number = str(r) + number
-    return number
-
+    if T > N:
+        return str(nums[N % T]) if N % T > 9 else str(N % T)
+    return notation(N // T, T) + (str(nums[N % T]) if N % T > 9 else str(N % T))
 
 num = int(input('Десятичное число: '))
 base = int(input('Основание (2-16): '))
